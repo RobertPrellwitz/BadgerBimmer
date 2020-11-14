b2939159ed82fdb4165124e1cb335a581f0994ff
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-11-14 08:00:40 -0600

    Identity implementation buildout

--- OktoberFast/OktoberFast/Startup.cs
+++ OktoberFast/OktoberFast/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
@@ -11,6 +12,7 @@ using OktoberFast.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace OktoberFast
@@ -35,6 +37,11 @@ namespace OktoberFast
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
+            services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(GlobalRouting));
+            });
 
             services.AddControllersWithViews();
             services.AddRazorPages();
