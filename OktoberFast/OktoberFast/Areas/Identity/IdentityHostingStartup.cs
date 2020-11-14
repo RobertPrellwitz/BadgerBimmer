5a9f77cdb08a6b57c15a41afe80160e8b31da16b
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-11-14 06:41:26 -0600

    Basic setup through page 7 of tutorial

---
+++ OktoberFast/OktoberFast/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OktoberFast.Data;
+
+[assembly: HostingStartup(typeof(OktoberFast.Areas.Identity.IdentityHostingStartup))]
+namespace OktoberFast.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
